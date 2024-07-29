8981482d039128f879bf765bccde37b962790adb
Teoslava Yordanova <>
2024-07-29 11:31:35 +0300

    React essentials project

---
+++ 01-starting-project/src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import reactImg from "../../assets/react-core-concepts.png";
+import "./Header.css";
+
+const reactDescription = ["Fundamental", "Crucial", "Core"];
+
+function genRandomIndex(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+export default function Header() {
+  const description = reactDescription[genRandomIndex(2)];
+  return (
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
